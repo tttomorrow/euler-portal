3f64498cd453212a52cab025c75a63356d53c67b
zhangshuai <1016868503@qq.com>
2022-07-21 17:23:42 +0800

    fix：修复了onMount的问题

--- app/.vitepress/src/views/brand/TheBrand.vue
+++ app/.vitepress/src/views/brand/TheBrand.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, Ref, ref } from 'vue';
+import { Ref, ref } from 'vue';
 import banner from '@/assets/banner-secondary.png';
 import search from '@/assets/illustrations/search.png';
 
