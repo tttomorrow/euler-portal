e00a0110dfe20a940d8e434afff66791d145b5ee
Bang <15622356989@163.com>
2022-08-26 16:33:05 +0800

    fix: fix bug

--- app/.vitepress/src/views/migration/TheMigrationDownLoad.vue
+++ app/.vitepress/src/views/migration/TheMigrationDownLoad.vue
@@ -7,13 +7,13 @@ const downloadInfo = [
     summary:
       'oec-hardware是保证 openEuler 与硬件平台的兼容性，验证仅限于基本功能验证，不包括性能测试等其它测试',
     version: '1.0.0',
-    soure: {
+    source: {
       name: '获取源码',
       link: 'https://gitee.com/openeuler/oec-hardware',
     },
     doc: {
       name: 'oec-hardware使用指南',
-      link: 'https://gitee.com/openeuler/oec-hardware',
+      link: 'https://gitee.com/openeuler/oec-hardware/blob/master/README.md',
     },
   },
   {
@@ -37,7 +37,7 @@ const downloadInfo = [
     summary:
       '软件评估，支持扫描并分析软件包、源码包，评估软件的兼容性及可移植性。配置评估，支持扫描并收集环境的操作系统配置信息，评估用户操作系统的配置兼容性。硬件评估，支持扫描并分析用户环境硬件信息，评估硬件兼容性。',
     version: '2.0.0',
-    soure: {
+    source: {
       name: '软件获取',
       link: 'https://repo.oepkgs.net/openEuler/rpm/openEuler-20.03-LTS-SP1/contrib/x2openEuler/',
     },
@@ -52,7 +52,7 @@ const downloadInfo = [
     summary:
       '1.检测2个ISO（基于RPM）的软件包，软件包内文件，库文件接口（C/C++）,内核KABI的变化差异，根据这些差异可以分析2个OS之间的兼容性，为软件移植提供了重要参考 2.OECP还可以检测同一个软件（RPM包）在不同版本下的变化以及差异，判断软件包的文件，接口等变化，可以分析得出软件不同版本之间的兼容性',
     version: '1.0.0',
-    soure: {
+    source: {
       name: '获取源码',
       link: 'https://toscode.gitee.com/openeuler/oecp/tree/master',
     },
@@ -93,15 +93,21 @@ const downloadInfo = [
             <div class="migration-link">
               <p class="migration-link-source">
                 <span>软件下载</span>
-                <a :style="{ marginLeft: '8px' }" :href="item.source?.link">{{
-                  item.source?.name
-                }}</a>
+                <a
+                  :style="{ marginLeft: '8px' }"
+                  :href="item.source?.link"
+                  target="_blank"
+                  >{{ item.source?.name }}</a
+                >
               </p>
               <p class="migration-link-doc">
                 <span>文档：</span>
-                <a :style="{ marginLeft: '8px' }" :href="item.doc?.link">{{
-                  item.doc?.name
-                }}</a>
+                <a
+                  :style="{ marginLeft: '8px' }"
+                  :href="item.doc?.link"
+                  target="_blank"
+                  >{{ item.doc?.name }}</a
+                >
               </p>
             </div>
           </div>
@@ -117,6 +123,10 @@ const downloadInfo = [
   font-size: var(--o-font-size-h7);
   line-height: var(--o-line-height-h7);
   font-weight: 300;
+  @media screen {
+    font-size: var(--o-font-size-h7);
+    line-height: var(--o-line-height-h7);
+  }
 }
 
 .migration-download {
@@ -145,6 +155,14 @@ const downloadInfo = [
       flex-direction: column;
       justify-content: space-between;
       min-height: 316px;
+      @media screen and (max-width: 1080px) {
+        min-height: 200px;
+      }
+
+      @media screen and (max-width: 768px) {
+        min-height: auto;
+      }
+
       .card-top {
         display: flex;
         flex-direction: column;
@@ -162,14 +180,27 @@ const downloadInfo = [
           .migration-summary {
             margin-top: var(--o-spacing-h8);
           }
+
+          @media screen and (max-width: 768px) {
+            font-size: var(--o-font-size-tip);
+            line-height: var(--o-line-height-tip);
+          }
         }
       }
       .card-bottom {
         margin-top: var(--o-spacing-h6);
+        @media screen and (max-width: 1080px) {
+          margin-top: var(--o-spacing-h4);
+        }
         .migration-version {
           font-size: var(--o-font-size-text);
           line-height: var(--o-line-height-text);
           color: var(--e-color-text4);
+
+          @media screen and (max-width: 768px) {
+            font-size: var(--o-font-size-tip);
+            line-height: var(--o-line-height-tip);
+          }
         }
 
         .migration-link {
@@ -186,6 +217,13 @@ const downloadInfo = [
             flex-direction: row;
           }
 
+          @media screen and (max-width: 768px) {
+            flex-direction: column;
+            margin-top: var(--o-spacing-h8);
+            font-size: var(--o-font-size-tip);
+            line-height: var(--o-line-height-tip);
+          }
+
           .migration-link-source {
             display: inline-block;
           }
@@ -200,6 +238,10 @@ const downloadInfo = [
             @media screen and (max-width: 1080px) {
               margin-left: var(--o-spacing-h3);
               margin-top: 0;
+            }
+            @media screen and (max-width: 768px) {
+              margin-left: 0;
+              margin-top: var(--o-spacing-h8);
             }
           }
         }
