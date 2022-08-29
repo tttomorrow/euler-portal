3f623a02e05034f1668cf447f64e03cc31601b38
ailoooong <ilong8@qq.com>
2022-08-29 16:52:52 +0800

    fix：secgear

--- app/.vitepress/src/views/other/projects/secgear/TheSecGear.vue
+++ app/.vitepress/src/views/other/projects/secgear/TheSecGear.vue
@@ -19,6 +19,7 @@ const i18n = useI18n();
     :subtitle="i18n.secgear.SECGEAR_BANNER_TEXT[1]"
     :illustration="illustration"
     :background-image="banner"
+    background-text="SEARCH"
   />
   <AppContent>
     <MiniDescription :description-desc="i18n.secgear.SECGEAR_DESC" />
