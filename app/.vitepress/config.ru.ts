67c059b13b73489ac8ff144280ff3ef7fb41063b
Bang <15622356989@163.com>
2022-09-20 11:47:58 +0800

    fix: 更新埋点

--- app/.vitepress/config.ru.ts
+++ app/.vitepress/config.ru.ts
@@ -20,7 +20,7 @@ const config: UserConfig = {
     [
       'script',
       {
-        src: '/js/analytics.js',
+        src: '/js/analytics-ru.js',
       },
     ],
     [
@@ -55,22 +55,10 @@ const config: UserConfig = {
   titleTemplate: false, //  vitepress supports pageTitileTemplate since 1.0.0
   locales: {
     '/': {
-      lang: 'zh',
+      lang: 'ru',
       title: 'openEuler',
       description:
         'openEuler 是一个开源、免费的 Linux 发行版平台，将通过开放的社区形式与全球的开发者共同构建一个开放、多元和架构包容的软件生态体系。同时，openEuler 也是一个创新的平台，鼓励任何人在该平台上提出新想法、开拓新思路、实践新方案。',
-    },
-    '/zh/': {
-      lang: 'zh',
-      title: 'openEuler',
-      description:
-        'openEuler 是一个开源、免费的 Linux 发行版平台，将通过开放的社区形式与全球的开发者共同构建一个开放、多元和架构包容的软件生态体系。同时，openEuler 也是一个创新的平台，鼓励任何人在该平台上提出新想法、开拓新思路、实践新方案。',
-    },
-    '/en/': {
-      lang: 'en',
-      title: 'openEuler',
-      description:
-        'openEuler is an open source, free Linux distribution platform. The platform provides an open community for global developers to build an open, diversified, and architecture-inclusive software ecosystem. openEuler is also an innovative platform that encourages everyone to propose new ideas, explore new approaches, and practice new solutions.',
     },
     '/ru/': {
       lang: 'ru',
@@ -80,13 +68,7 @@ const config: UserConfig = {
     },
   },
   themeConfig: {
-    // locales: {
-    //   '/': i18n.zh,
-    //   '/zh/': i18n.zh,
-    //   '/en/': i18n.en,
-    //   '/ru/': i18n.ru,
-    // },
-    docsUrl: 'https://docs.openeuler.org',
+    docsUrl: 'https://docs.ru.openeuler.org',
   },
 };
 export default config;
