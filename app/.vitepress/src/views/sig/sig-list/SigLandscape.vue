3d904bafb9bc27203ee87dc7f5a87108fd2b6d55
Bang <15622356989@163.com>
2022-09-29 10:36:06 +0800

    feat: SigLandscapeFeature封装

--- app/.vitepress/src/views/sig/sig-list/SigLandscape.vue
+++ app/.vitepress/src/views/sig/sig-list/SigLandscape.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { useData, useRouter } from 'vitepress';
 import { onMounted, ref } from 'vue';
+
+import SigLandscapeFeature from './SigLandscapeFeature.vue';
+
 import { getSigLandscape } from '@/api/api-sig';
+
+import { GroupInfo } from '@/shared/@types/type-sig';
 
 const configData = useData();
 const router = useRouter();
@@ -11,64 +16,11 @@ const toSigDetail = (name: string): void => {
   router.go(`/${language.value}/sig/sig-detail/?name=${name}`);
 };
 
-const landscapeInfo = ref({});
+const landscapeInfo = ref<GroupInfo[]>([]);
 
 onMounted(async () => {
   try {
-    const res = await getSigLandscape();
-    const info: any = [];
-    for (let i = 0, len = res.data?.length; i < len; i++) {
-      const item = res.data[i];
-      if (item.group === '' && item.feature === '') {
-        continue;
-      }
-
-      if (
-        !info.find((group: any) => {
-          return group.groupName === item.group;
-        })
-      ) {
-        info.push({
-          groupName: item.group,
-          features: [],
-        });
-      }
-
-      const groupInfo = info.find((group: any) => {
-        return group.groupName === item.group;
-      });
-
-      if (
-        !groupInfo?.features.find((feature: any) => {
-          return feature.featureName === item.feature;
-        })
-      ) {
-        groupInfo?.features.push({
-          featureName: item.feature,
-          sigs: [],
-        });
-      }
-
-      const featureInfo = groupInfo?.features.find((feature: any) => {
-        return feature.featureName === item.feature;
-      });
-      featureInfo.sigs.push(item.sig_names);
-    }
-    info.sort((b: any, a: any) => {
-      return a.features.length - b.features.length;
-    });
-    info.forEach((group: any) => {
-      group.features.sort((b: any, a: any) => {
-        return a.sigs.length - b.sigs.length;
-      });
-      group.features.forEach((feature: any) => {
-        feature.sigs.sort((b: any, a: any) => {
-          return b.localeCompare(a);
-        });
-      });
-    });
-
-    landscapeInfo.value = info;
+    landscapeInfo.value = await getSigLandscape();
   } catch (err: any) {
     throw new Error(err);
   }
@@ -82,26 +34,11 @@ onMounted(async () => {
       :key="group.groupName"
       class="landscape-group"
     >
-      <p class="landscape-group-title">{{ group?.groupName }}</p>
-      <div class="landscape-feature">
-        <div
-          v-for="feature in group?.features"
-          :key="feature.featureName"
-          class="landscape-feature-item"
-        >
-          <div class="feature-item-title">{{ feature.featureName }}</div>
-          <ul class="feature-item-sig">
-            <li
-              v-for="sig in feature.sigs"
-              :key="sig"
-              class="sig-item"
-              @click="toSigDetail(sig)"
-            >
-              {{ sig }}
-            </li>
-          </ul>
-        </div>
-      </div>
+      <p class="landscape-group-title">{{ group.groupName }}</p>
+      <SigLandscapeFeature
+        :info="group?.features"
+        @sig-click="toSigDetail"
+      ></SigLandscapeFeature>
     </div>
   </div>
 </template>
@@ -122,143 +59,36 @@ onMounted(async () => {
       color: var(--o-color-text1);
     }
 
-    .landscape-feature {
-      display: grid;
-      grid-template-columns: repeat(7, 1fr);
-      column-gap: var(--o-spacing-h6);
-      .landscape-feature-item {
+    &:nth-child(2) {
+      :deep(.landscape-feature .landscape-feature-item) {
         &:nth-child(1) {
           .feature-item-title {
-            background-color: #7f6bbe;
+            background-color: #8e583d;
           }
         }
         &:nth-child(2) {
           .feature-item-title {
-            background-color: #4d7dff;
+            background-color: #73c0de;
           }
         }
         &:nth-child(3) {
           .feature-item-title {
-            background-color: #db7c61;
+            background-color: #cec79a;
           }
         }
         &:nth-child(4) {
           .feature-item-title {
-            background-color: #8e9aaf;
+            background-color: #4c3e72;
           }
         }
         &:nth-child(5) {
           .feature-item-title {
-            background-color: #505d75;
+            background-color: #19647e;
           }
         }
         &:nth-child(6) {
           .feature-item-title {
-            background-color: #2a9d8f;
-          }
-        }
-        &:nth-child(7) {
-          .feature-item-title {
-            background-color: #fec456;
-          }
-        }
-
-        .feature-item-title {
-          padding: 18px;
-          font-size: var(--o-font-size-h8);
-          line-height: var(--o-line-height-h8);
-          color: var(--o-color-white);
-          text-align: center;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          overflow: hidden;
-        }
-
-        .feature-item-sig {
-          margin-top: var(--o-spacing-h5);
-          .sig-item {
-            & + .sig-item {
-              margin-top: var(--o-spacing-h5);
-            }
-
-            text-align: center;
-            padding: 0 23px;
-            height: 46px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-size: var(--o-font-size-text);
-            line-height: var(--o-line-height-text);
-            color: #002fa7;
-            border: 1px solid #002fa7;
-            background-color: #ffffff;
-            transition: all 0.3s;
-            cursor: pointer;
-
-            &:hover {
-              background: #002fa7;
-              color: #ffffff;
-              transform: scale(1.05, 1.05);
-            }
-          }
-        }
-      }
-    }
-
-    &:nth-child(2) {
-      .landscape-feature {
-        .landscape-feature-item {
-          &:nth-child(1) {
-            .feature-item-title {
-              background-color: #8e583d;
-            }
-          }
-          &:nth-child(2) {
-            .feature-item-title {
-              background-color: #73c0de;
-            }
-          }
-          &:nth-child(3) {
-            .feature-item-title {
-              background-color: #cec79a;
-            }
-          }
-          &:nth-child(4) {
-            .feature-item-title {
-              background-color: #4c3e72;
-            }
-          }
-          &:nth-child(5) {
-            .feature-item-title {
-              background-color: #19647e;
-            }
-          }
-          &:nth-child(6) {
-            .feature-item-title {
-              background-color: #c44e4e;
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-.dark {
-  .landscape-group {
-    .landscape-feature {
-      .landscape-feature-item {
-        .feature-item-sig {
-          .sig-item {
-            color: #ffffff;
-            background: #383838;
-            border: 1px solid #b2b2b2;
-
-            &:hover {
-              background: #406fe7;
-              color: #ffffff;
-              border-color: #406fe7;
-            }
+            background-color: #c44e4e;
           }
         }
       }
