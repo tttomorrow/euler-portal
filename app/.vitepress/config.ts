ba12014d7b8af8e22801e6b012f737456f132595
Bang <15622356989@163.com>
2022-11-01 00:35:29 +0800

    fix: fix bug for search

--- app/.vitepress/config.ts
+++ app/.vitepress/config.ts
@@ -19,6 +19,14 @@ const config: UserConfig = {
       },
     ],
     [
+      'meta',
+      {
+        name: 'keywords',
+        content:
+          'openEuler,开源Linux系统,linux开源社区,开源社区,Linux迁移,EulerOS',
+      },
+    ],
+    [
       'script',
       {
         src: '/js/analytics.js',
@@ -36,7 +44,6 @@ const config: UserConfig = {
         src: '/allow_sensor/sensors.js',
       },
     ],
-
     [
       'meta',
       {
@@ -53,32 +60,12 @@ const config: UserConfig = {
       title: 'openEuler',
       description:
         'openEuler 是一个开源、免费的 Linux 发行版平台，将通过开放的社区形式与全球的开发者共同构建一个开放、多元和架构包容的软件生态体系。同时，openEuler 也是一个创新的平台，鼓励任何人在该平台上提出新想法、开拓新思路、实践新方案。',
-      head: [
-        [
-          'meta',
-          {
-            name: 'keywords',
-            content:
-              'openEuler,开源Linux系统,linux开源社区,开源社区,Linux迁移,EulerOS',
-          },
-        ],
-      ],
     },
     '/zh/': {
       lang: 'zh',
       title: 'openEuler',
       description:
         'openEuler 是一个开源、免费的 Linux 发行版平台，将通过开放的社区形式与全球的开发者共同构建一个开放、多元和架构包容的软件生态体系。同时，openEuler 也是一个创新的平台，鼓励任何人在该平台上提出新想法、开拓新思路、实践新方案。',
-      head: [
-        [
-          'meta',
-          {
-            name: 'keywords',
-            content:
-              'openEuler,开源Linux系统,linux开源社区,开源社区,Linux迁移,EulerOS',
-          },
-        ],
-      ],
     },
     '/en/': {
       lang: 'en',
