4fa01abfc000d029a8ace7243ea8392e788b7d8d
lishengbo <1171734220@qq.com>
2022-11-02 18:29:43 +0800

    feat:迁移方案添加icon

--- app/.vitepress/src/data/migration/migration-toc.ts
+++ app/.vitepress/src/data/migration/migration-toc.ts
@@ -3,53 +3,10 @@ export default [
     label: '背景',
     link: 'background',
   },
-  //
   {
     label: '迁移优势',
     link: 'advantage',
   },
-  // {
-  //   label: '方案介绍',
-  //   link: 'guidance',
-  //   children: [
-  //     {
-  //       label: '迁移概述',
-  //       link: '1-summary',
-  //     },
-  //     {
-  //       label: '迁移注意事项',
-  //       link: '2-attention',
-  //     },
-  //     {
-  //       label: '迁移流程',
-  //       link: '3-process',
-  //     },
-  //     {
-  //       label: '迁移评估',
-  //       link: '4-assessment',
-  //     },
-  //     {
-  //       label: '迁移适配',
-  //       link: '5-adaptive',
-  //     },
-  //     {
-  //       label: '迁移实施（存量替换）',
-  //       link: '6-stock-migration',
-  //     },
-  //     {
-  //       label: '迁移实施（新增/扩容）',
-  //       link: '7-increased-migration',
-  //     },
-  //     {
-  //       label: '使用SUT进行升级（原地升级）',
-  //       link: '8-updated',
-  //     },
-  //     {
-  //       label: '跟踪迁移',
-  //       link: '9-followup',
-  //     },
-  //   ],
-  // },
   {
     label: '迁移方案',
     link: 'guidance',
@@ -59,10 +16,6 @@ export default [
     link: 'download',
     isLeaf: true,
   },
-  // {
-  //   label: '用户案例',
-  //   link: 'user-cases',
-  // },
   {
     label: '最佳实践',
     link: 'user-cases',
