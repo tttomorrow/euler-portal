be2d869bf8e75ba86027a9a45d8087055fe363ef
chenHusky <377075038@qq.com>
2022-11-25 16:29:08 +0800

    no ru

--- app/.vitepress/src/i18n/common/common-ru.ts
+++ app/.vitepress/src/i18n/common/common-ru.ts
@@ -282,10 +282,6 @@ export default {
   ],
   LANG: 'Русский',
   LANG_LIST: ['中文', 'English', 'Русский'],
-  USER_CENTER: 'Личный центр',
-  LOGOUT: 'выход',
-  PLEASE: 'Please',
-  LOGIN: 'login',
   GITTE: 'Source code',
   CODE: 'Код',
   FOOTER: {
