74d5fd0077cfb76bc3c7529a2fd69b93ba9a8553
haml-707 <845831435@qq.com>
2022-12-07 19:13:14 +0800

    fix：暂时下线浏览量

--- app/.vitepress/src/views/blog/TheBlog.vue
+++ app/.vitepress/src/views/blog/TheBlog.vue
@@ -568,7 +568,7 @@ const moblieCurrentChange = (val: string) => {
                   {{ item.archives }}
                 </p>
               </div>
-              <div class="infodetail">
+              <div v-if="false" class="infodetail">
                 <OIcon class="icon"><IconBrowse /></OIcon>
                 <p>
                   {{ userCaseData.BROWSE }}{{ item.views
